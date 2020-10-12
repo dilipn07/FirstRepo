d907826baafe58063f7ef9a4cd9de4589471d4d9
Dilip kumar Nomula <dilip@Dilips-MacBook-Pro.local>
2020-10-11 21:34:14 -0500

    xcode project

---
+++ FirstRepo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstRepo
+//
+//  Created by Dilip kumar Nomula on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstRepo/FirstRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstRepoApp.swift
+//  FirstRepo
+//
+//  Created by Dilip kumar Nomula on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FirstRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
